bb5de0c685166a604ae5038a15f4ce25b2c5fda8
Tran Kiet <anhkiet10h1nd@gmail.com>
2024-06-11 17:36:39 +0700

    fix fetch api

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,13 +1,15 @@
-export const baseUrl = "http://127.0.0.1:8000/api";
+import {jwtDecode} from "jwt-decode"
 
-export const postRequest = async(url, body) => {
+export const baseUrl = "http://127.0.0.1:8000/api";
+export const refreshUrl = "http://127.0.0.1:8000/api/token/refresh/";
+
+export const postLoginOrRegister = async (url, body) => {
     const response = await fetch(url, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.token}` 
         },
-        credentials: "include",
+        credentials: 'include',
         body
     });
     const data = await response.json();
@@ -21,15 +23,81 @@ export const postRequest = async(url, body) => {
 
         return { error: true, message };
     }
+    return data;
+}
+
+export const postRequest = async(url, body) => {
+    let date = new Date();
+    const decodedToken = jwtDecode(localStorage.token);
+    if (decodedToken.exp < date.getTime() / 1000) {
+        const res = await axios.get(refreshUrl, {
+            withCredentials: true,
+        })
+
+        if (!res.ok) {
+            let message;
+            if (data?.detail)
+                message = data.detail;
+            else
+                message = data;
+    
+            return { error: true, message };
+        }
+
+        localStorage.removeItem('token');
+        localStorage.setItem('token', res['accessToken']);
+    };
+
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Token": `Bearer ${localStorage.token}` 
+        },
+        credentials: "include",
+        body
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+        let message;
+        if (data?.detail)
+            message = data.detail;
+        else
+            message = data;
+
+        return { error: true, message };
+    }
 
     return data;
 };
 
 export const getRequest = async (url) => {
+    let date = new Date();
+    const decodedToken = jwtDecode(localStorage.token);
+    if (decodedToken.exp < date.getTime() / 1000) {
+        const res = await fetch(refreshUrl, {
+            credentials: 'include'
+        })
+
+        localStorage.removeItem('token');
+        localStorage.setItem('token', res['accessToken']);
+
+        if (!res.ok) {
+            let message;
+            if (data?.detail)
+                message = data.detail;
+            else
+                message = data;
+    
+            return { error: true, message };
+        }
+    };
+
     const response = await fetch(url, {
         credentials: 'include',
         headers: {
-            Authorization: `Bearer ${localStorage.token}` 
+            "Token": `Bearer ${localStorage.token}` 
         }
     });
 
