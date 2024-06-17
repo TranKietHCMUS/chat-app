32a4261f0348e5eb1b302d40ba4876e81f3cec6d
Tran Kiet <anhkiet10h1nd@gmail.com>
2024-06-17 07:22:07 +0700

    add chat feature

--- frontend/src/components/Chat/UserChat.jsx
+++ frontend/src/components/Chat/UserChat.jsx
@@ -1,4 +1,6 @@
 import { Stack } from "react-bootstrap";
+import male from "../../assets/male.svg"
+import female from "../../assets/female.svg"
 
 const UserChat = ({chat, user}) => {
     return (
@@ -6,10 +8,11 @@ const UserChat = ({chat, user}) => {
             direction="horizontal"
             gap={3}
             className="user-card align-items-center p-2 justify-content-between"
+            role="button"
         >
             <div className="d-flex">
                 <div className="me-2">
-                        
+                    {chat?.gender == "Male" ? <img src={male} height="35px"></img> : <img src={female} height="35px"></img>}
                 </div>
                 <div className="text-content">
                     <div className="name">{chat?.first_name + " " + chat?.last_name}</div>
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,11 +3,12 @@ import { ChatContext } from "../context/ChatContext";
 import { Container, Stack } from "react-bootstrap";
 import UserChat from "../components/Chat/UserChat";
 import { AuthContext } from "../context/AuthContext";
+import ChatBox from "../components/Chat/ChatBox";
 
 
 export default function ChatPage() {
     const {user} = useContext(AuthContext);
-    const {userChats, isUserChatsLoading, userChatsError, getUserChats} = useContext(ChatContext);
+    const {userChats, isUserChatsLoading, userChatsError, getUserChats, updateCurrentChat} = useContext(ChatContext);
 
     useEffect(() => {
         getUserChats()
@@ -21,13 +22,13 @@ export default function ChatPage() {
                     {isUserChatsLoading && <p>Loading chats...</p>}
                     {userChats?.map((chat, index) => {
                         return (
-                            <div key={index}>
+                            <div key={index} onClick = {() => updateCurrentChat(chat)}>
                                 <UserChat chat={chat} user={user}/>
                             </div>
                         )
                     })}
                 </Stack>
-                <p>Chat Box</p>
+                <ChatBox />
             </Stack>)}
         </Container>    
     );
